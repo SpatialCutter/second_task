aeeaaf974737dd3edb3aef97f743a719bd112637
Данил Сергеевич Брестель <latum@internet.ru>
2025-04-09 19:10:04 +0600

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+// для поиска react-bootstrap bootstrap
+
+function App() {
+  return (
+    <>
+      <div>Задание выполнять тут</div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/task.md
@@ -0,0 +1,5 @@
+![alt text](image.png)
+
+https://www.pokemon.com/uk/pokedex/golurk
+
+![alt text](image-1.png)
